86a5dbd0b464e02c94421c77d4d72e6f70c2cf0b
MariiaZheliezna <iron_mary@icloud.com>
2023-10-15 18:05:57 +0200

    v1.0

--- main.py
+++ main.py
@@ -1,19 +1,5 @@
-message = input("Enter a message: ")
-offset = int(input("Enter the offset: "))
-offset = offset % 26
-encoded_message = ""
-for ch in message:
-    if (ord(ch) <= ord("z") and ord(ch) >= ord("a")):  # проверка на то, что это символы a-z A-Z
-        pos = ord(ch) - ord("a")
-        pos = pos + offset - 26*((pos+offset)//26)
-        encoded_message = encoded_message + chr(pos + ord("a"))
-    elif (ord(ch) <= ord("Z") and ord(ch) >= ord("A")):
-        pos = ord(ch) - ord("A")
-        pos = pos + offset - 26*((pos+offset)//26)
-        encoded_message = encoded_message + chr(pos + ord("A"))        
-    else:
-        encoded_message = encoded_message + ch 
-print(encoded_message)
+# Test
+
 
        
     
